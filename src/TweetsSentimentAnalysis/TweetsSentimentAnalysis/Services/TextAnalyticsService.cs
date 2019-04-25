4a4dfbba975914c8e1b6888cc794527b0a094f91
hinault <ngdonfack@gmail.com>
2019-04-25 17:35:51 -0400

    Implement Interface

--- src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/TextAnalyticsService.cs
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/TextAnalyticsService.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TweetsSentimentAnalysis.Models;
 
 namespace TweetsSentimentAnalysis.Services
 {
-    public class TextAnalyticsService
+    public class TextAnalyticsService : ITextAnalyticsService
     {
+        public Task<List<ResultModel>> Language(List<ResultModel> result)
+        {
+            throw new NotImplementedException();
+        }
 
-
+        public Task<List<ResultModel>> Sentiment(List<ResultModel> result)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
