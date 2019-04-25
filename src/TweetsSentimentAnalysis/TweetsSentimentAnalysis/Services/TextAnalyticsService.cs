8d73ff75c8739d4b35b76bfa051c1bf0f72de8d9
hinault <ngdonfack@gmail.com>
2019-04-25 17:35:13 -0400

    Add TextAnalyticsService class

---
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/TextAnalyticsService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetsSentimentAnalysis.Services
+{
+    public class TextAnalyticsService
+    {
+
+
+    }
+}
