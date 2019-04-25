5d55ba6574f1d4b5defc0fc06bd36d4bab51901d
hinault <ngdonfack@gmail.com>
2019-04-25 17:34:02 -0400

    Add ITextAnalyticsService

---
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/ITextAnalyticsService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TweetsSentimentAnalysis.Models;
+
+namespace TweetsSentimentAnalysis.Services
+{
+    interface ITextAnalyticsService
+    {
+        Task<List<ResultModel>> Sentiment(List<ResultModel> result);
+
+        Task<List<ResultModel>> Language(List<ResultModel> result);
+    }
+}
