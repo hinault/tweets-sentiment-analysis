b2f6c1ee4ccfdf16dde56f20b299b18820cc0e68
hinault <ngdonfack@gmail.com>
2019-04-25 16:20:03 -0400

    add getTweets method

--- src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/TweetsSearch.cs
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/TweetsSearch.cs
@@ -2,10 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Tweetinvi;
+using Tweetinvi.Models;
 
 namespace TweetsSentimentAnalysis.Services
 {
     public class TweetsSearch
     {
+
+        public IList<string> GetTweets(string tag)
+        {
+            var searchParameter = Search.CreateTweetSearchParameter("#my_tag");
+
+            
+            searchParameter.SearchType = SearchResultType.Popular;
+            searchParameter.MaximumNumberOfResults = 200;
+            
+
+            var tweets = Search.SearchTweets(searchParameter);
+
+          return  tweets.Select(x => x.FullText).ToList();
+        }
     }
 }
