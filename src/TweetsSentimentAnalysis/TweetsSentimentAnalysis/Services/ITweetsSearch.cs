e31e6b7b45df7d93b84f03aed04c3eba180c2aa3
hinault <ngdonfack@gmail.com>
2019-04-25 16:04:44 -0400

    add ITweetsSearch interface

---
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Services/ITweetsSearch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetsSentimentAnalysis.Services
+{
+    interface ITweetsSearch
+    {
+    }
+}
