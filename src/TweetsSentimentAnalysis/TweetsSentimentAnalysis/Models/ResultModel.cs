45cf786a5eb655ed88b9439ceca366f8d6d604dd
hinault <ngdonfack@gmail.com>
2019-04-25 17:30:11 -0400

    Add ResultModel

---
+++ src/TweetsSentimentAnalysis/TweetsSentimentAnalysis/Models/ResultModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TweetsSentimentAnalysis.Models
+{
+    public class ResultModel
+    {
+        public string Text { get; set; }
+        public string Language { get; set; }
+        public string Score { get; set; }
+    }
+}
